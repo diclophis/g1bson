d0e4c5c6d66b883833434820d3dc473c8617961a
Diclophis <diclophis@gmail.com>
2014-01-29 20:55:47 -0800

    make new master device

--- g1bson-osx/xinput2_test.c
+++ g1bson-osx/xinput2_test.c
@@ -49,7 +49,9 @@ static void select_events(Display *dpy, Window win)
 
     memset(mask1, 0, sizeof(mask1));
 
+    
     /* select for button and key events from all master devices */
+    /*
     XISetMask(mask1, XI_ButtonPress);
     XISetMask(mask1, XI_ButtonRelease);
     XISetMask(mask1, XI_KeyPress);
@@ -58,19 +60,152 @@ static void select_events(Display *dpy, Window win)
     evmasks[0].deviceid = XIAllMasterDevices;
     evmasks[0].mask_len = sizeof(mask1);
     evmasks[0].mask = mask1;
+    */
 
-    memset(mask2, 0, sizeof(mask2));
 
     /* Select for motion from the default cursor */
+    /*
+    memset(mask2, 0, sizeof(mask2));
     XISetMask(mask2, XI_Motion);
 
-    evmasks[1].deviceid = 2; /* the default cursor */
+    evmasks[1].deviceid = 2;
     evmasks[1].mask_len = sizeof(mask2);
     evmasks[1].mask = mask2;
+    */
 
-    XISelectEvents(dpy, win, evmasks, 2);
+    XISetMask(mask1, XI_HierarchyChanged);
+    evmasks[0].mask = mask1;
+    evmasks[0].mask_len = sizeof(mask1);
+    evmasks[0].deviceid = XIAllDevices;
+
+    XISelectEvents(dpy, win, evmasks, 1);
     XFlush(dpy);
 }
+
+static void list_devices(Display *display, int deviceid)
+{
+    XIDeviceInfo *info, *dev;
+    int ndevices;
+    int i;
+    const char *type = "";
+
+    info = XIQueryDevice(display, deviceid, &ndevices);
+
+    for(i = 0; i < ndevices; i++) {
+        dev = &info[i];
+
+        printf("'%s' (%d)\n", dev->name, dev->deviceid);
+        switch(dev->use) {
+            case XIMasterPointer: type = "master pointer"; break;
+            case XIMasterKeyboard: type = "master keyboard"; break;
+            case XISlavePointer: type = "slave pointer"; break;
+            case XISlaveKeyboard: type = "slave keyboard"; break;
+            case XIFloatingSlave: type = "floating slave"; break;
+        }
+
+        printf(" - is a %s\n", type);
+        printf(" - current pairing/attachment: %d\n", dev->attachment);
+        if (!dev->enabled)
+            printf(" - this device is disabled!\n");
+    }
+
+    XIFreeDeviceInfo(info);
+}
+
+static void create_remove_master(Display *dpy, int xi_opcode)
+{
+    XIAddMasterInfo add;
+    XIRemoveMasterInfo remove;
+    XEvent ev;
+    int new_master_id;
+
+    add.type = XIAddMaster;
+    add.name = "My new master";
+    add.send_core = True;
+    add.enable = True;
+
+    XIChangeHierarchy(dpy, (XIAnyHierarchyChangeInfo*)&add, 1);
+    XFlush(dpy);
+
+    //printf("New master device:");
+    //list_devices(dpy, add->deviceid);
+    //new_master_id = add.deviceid;
+
+    /*
+    unsigned char mask[2] = { 0, 0 };
+    XIEventMask evmask;
+
+    evmask.mask = mask;
+    evmask.mask_len = sizeof(mask);
+    evmask.deviceid = XIAllDevices;
+
+
+    XISetMask(mask, XI_HierarchyChanged);
+    XISelectEvents(dpy, DefaultRootWindow(dpy), &evmask, 1);
+    //XISelectEvents(dpy, 0, &evmask, 1);
+    */
+
+    while(1)
+    {
+        XGenericEventCookie *cookie = &ev.xcookie;
+
+        printf("?\n");
+
+        XNextEvent(dpy, (XEvent*)&ev);
+
+        printf(".\n");
+
+        if (cookie->type != GenericEvent ||
+            cookie->extension != xi_opcode ||
+            !XGetEventData(dpy, cookie)) {
+          printf("cont\n");
+          continue;
+        }
+
+        if (cookie->evtype == XI_HierarchyChanged)
+        {
+        printf("wtf\n");
+            XIHierarchyEvent *event = cookie->data;
+            if ((event->flags & XIMasterAdded))
+            {
+                int i;
+                XIHierarchyInfo *info;
+
+                for (i = 0; i < event->num_info; i++)
+                {
+                    info = &event->info[i];
+                    if (info->flags & XIMasterAdded)
+                    {
+                        printf("New master device:");
+                        list_devices(dpy, info->deviceid);
+
+                        new_master_id = info->deviceid;
+                    }
+                }
+
+                XFreeEventData(dpy, cookie);
+                break;
+            }
+        } else {
+          printf(".");
+        }
+
+        XFreeEventData(dpy, cookie);
+    }
+
+
+    printf("Removing new master devices again.\n");
+
+    remove.type = XIRemoveMaster;
+    remove.deviceid = new_master_id;
+    remove.return_mode = XIAttachToMaster;
+    remove.return_pointer = 2; /* VCP */
+    remove.return_keyboard = 3; /* VCK */
+
+    XIChangeHierarchy(dpy, (XIAnyHierarchyChangeInfo*)&remove, 1);
+    XFlush(dpy);
+}
+
 
 int main (int argc, char **argv)
 {
@@ -94,26 +229,37 @@ int main (int argc, char **argv)
     if (!has_xi2(dpy))
         return -1;
 
-    /* Create a simple window */
-    win = create_win(dpy);
+win = create_win(dpy);
 
-    /* select for XI2 events */
     select_events(dpy, win);
 
-    while(1) {
-        XGenericEventCookie *cookie = &ev.xcookie;
+    if (1) {
+      list_devices(dpy, XIAllDevices);
+      create_remove_master(dpy, xi_opcode);
+    }
 
-        XNextEvent(dpy, &ev);
+    if (0) {
+      /* Create a simple window */
+      win = create_win(dpy);
 
-        if (cookie->type != GenericEvent ||
-            cookie->extension != xi_opcode)
-            continue;
+      /* select for XI2 events */
+      select_events(dpy, win);
 
-        if (XGetEventData(dpy, cookie))
-        {
-            printf("Event type %d received\n", cookie->evtype);
-            XFreeEventData(dpy, &ev.xcookie);
-        }
+      while(1) {
+          XGenericEventCookie *cookie = &ev.xcookie;
+
+          XNextEvent(dpy, &ev);
+
+          if (cookie->type != GenericEvent ||
+              cookie->extension != xi_opcode)
+              continue;
+
+          if (XGetEventData(dpy, cookie))
+          {
+              printf("Event type %d received\n", cookie->evtype);
+              XFreeEventData(dpy, &ev.xcookie);
+          }
+      }
     }
 
     return 0;
